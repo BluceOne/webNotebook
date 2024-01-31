da1d9de0480a9593b1479881eebe7d5c683d14de
BluceOne <48558114+BluceOne@users.noreply.github.com>
2024-01-31 23:15:45 +0800

    修改vuepress 配置文件

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,6 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/docs/",
   lang: "zh-CN",
   theme,
   shouldPrefetch: false,
