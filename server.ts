e9a812374ab9ae31aa9a679a77f0b23cdaed340d
BluceOne <48558114+BluceOne@users.noreply.github.com>
2024-01-31 14:57:52 +0000

    更新server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 // index.js
 import { serve } from 'https://deno.land/std@0.140.0/http/server.ts'
-import { Request } from 'https://deno.land/std@0.140.0/http/server.ts';
 
-serve({ port: 8080 }, async (req: Request) => {
+serve({ port: 8080 }, async (req: any) => {
   await req.respond({
     status: 200,
     body: Deno.readTextFile('./index.html')
